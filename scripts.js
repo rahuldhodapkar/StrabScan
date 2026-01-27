e3caf131ab2c36544e5c085e2789279aa071ec32
Rahul Dhodapkar <rahul.m.dhodapkar@gmail.com>
2026-01-26 16:24:46 -0800

    save with better colors

--- scripts.js
+++ scripts.js
@@ -1391,42 +1391,42 @@ async function predictWebcam() {
       drawingUtils.drawConnectors(
         landmarks,
         FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE,
-        { color: "#FF3030" }
+        { color: "#C0C0C070" }
       )
       drawingUtils.drawConnectors(
         landmarks,
         FaceLandmarker.FACE_LANDMARKS_RIGHT_EYEBROW,
-        { color: "#FF3030" }
+        { color: "#C0C0C070" }
       )
       drawingUtils.drawConnectors(
         landmarks,
         FaceLandmarker.FACE_LANDMARKS_LEFT_EYE,
-        { color: "#30FF30" }
+        { color: "#C0C0C070" }
       )
       drawingUtils.drawConnectors(
         landmarks,
         FaceLandmarker.FACE_LANDMARKS_LEFT_EYEBROW,
-        { color: "#30FF30" }
+        { color: "#C0C0C070" }
       )
       drawingUtils.drawConnectors(
         landmarks,
         FaceLandmarker.FACE_LANDMARKS_FACE_OVAL,
-        { color: "#E0E0E0" }
+        { color: "#C0C0C070" }
       )
       drawingUtils.drawConnectors(
         landmarks,
         FaceLandmarker.FACE_LANDMARKS_LIPS,
-        { color: "#E0E0E0" }
+        { color: "#C0C0C070" }
       )
       drawingUtils.drawConnectors(
         landmarks,
         FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS,
-        { color: "#FF3030" }
+        { color: "#C0C0C070" }
       )
       drawingUtils.drawConnectors(
         landmarks,
         FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS,
-        { color: "#30FF30" }
+        { color: "#C0C0C070" }
       )
     }
   }
